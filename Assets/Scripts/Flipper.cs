056a84d13cf0738e6067a9f073e34cf952dea4b3
watanabe@marida <watanabe.everystudio@gmail.com>
2023-01-01 14:50:59 +0900

    フリッパー追加 一旦保存

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    [SerializeField] private HingeJoint2D myHingeJoint;
+    void Update()
+    {
+        myHingeJoint.useMotor = Input.GetMouseButton(1);
+    }
+}
