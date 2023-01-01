45d6f547281cf9c42f9382d8dc7efeb3358dd1a6
watanabe@marida <watanabe.everystudio@gmail.com>
2023-01-01 13:59:23 +0900

    当たり判定修正 ボールを打ち出してみる

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    [SerializeField] private float shootPower = 10f;
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myRigidbody.AddForce(Vector2.up * shootPower, ForceMode2D.Impulse);
+        }
+    }
+}
