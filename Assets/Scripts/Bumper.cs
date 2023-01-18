767b004edfefb6bf68e1c668c2f680a90baeaca6
watanabe@marida <watanabe.everystudio@gmail.com>
2023-01-19 00:34:42 +0900

    バンパーのアニメーションを追加

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Bumper : MonoBehaviour
 {
+    [SerializeField] private Animator myAnimator;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
+            myAnimator.SetTrigger("Shake");
             Debug.Log("Hit");
         }
     }
