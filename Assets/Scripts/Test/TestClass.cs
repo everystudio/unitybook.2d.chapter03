5d49e6743e92e0881a569b9c80b392ecc46d15a9
watanabe@marida <watanabe.everystudio@gmail.com>
2023-01-18 09:45:26 +0900

    バンパークラスを追加 ボールタグを追加

---
+++ Assets/Scripts/Test/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SaveData
+{
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+    public int testParam;
+}
+
+public class TestClass : MonoBehaviour
+{
+    private SaveData mySaveData;
+    private void Start()
+    {
+        SaveData saveData = new SaveData();
+        saveData.testParam = 5;
+        TempSaveData(saveData);
+
+        saveData.testParam = 10;
+        Debug.Log(mySaveData.testParam);
+    }
+    private void TempSaveData(SaveData tempSaveData)
+    {
+        //mySaveData = new SaveData(tempSaveData);
+        mySaveData = (SaveData)tempSaveData.Clone();
+    }
+}
